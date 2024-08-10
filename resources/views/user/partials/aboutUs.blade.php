27d85182f3811fe79d8c0f5755e27be3ce1b4bb9
KARUNONIH <gibran.anggalana21@gmail.com>
2024-08-10 17:40:04 +0700

    fe bento cake

---
+++ resources/views/user/partials/aboutUs.blade.php
@@ -0,0 +1,45 @@
+<div class="flex flex-col mt-10 w-[90%] md:w-full items-center justify-center mx-auto">
+    <section class="flex items-center flex-col md:flex-row">
+        <div class="md:w-1/2">
+            <img src="{{ asset('image/Bolu-Kukus-Lapis-talas.jpg') }}" alt=""
+                class=" aspect-video md:rounded-l-full md:border-8 md:border-r-0 border-[#ff78c4] md:p-2 pr-0 ">
+        </div>
+        <div class="md:w-1/2 md:pl-4 mt-4 mb-6 md:mt-0 md:mb-0">
+            <section class="flex gap-2 md:flex-col md:block items-center justify-center mb-3 md:mb-0">
+                <div class="md:w-10 md:h-10 w-8 h-8 bg-gray-100 flex items-center justify-center rounded-md shadow"><i
+                        class="fa-solid fa-timeline text-base md:text-xl text-[#ff78c4] "></i></div>
+                <h2 class="text-base md:text-xl font-bold md:mt-4 md:mb-6 capitalize">Kue Mini dengan Sentuhan Personal</h2>
+            </section>
+            <p class="text-xs text-justify md:text-left">Bento Cake adalah kue dessert mini yang didesain dengan ukuran kecil namun penuh keunikan. Keistimewaan utama dari Bento Cake adalah kemampuan untuk memesan desain sesuai dengan keinginan konsumen. Ini memungkinkan setiap pelanggan memiliki kue yang benar-benar personal dan spesial, disesuaikan dengan tema atau preferensi pribadi mereka.
+            </p>
+        </div>
+    </section>
+    <section class="flex items-center md:flex-row-reverse md:-mt-2 flex-col">
+        <div class="md:w-1/2">
+            <img src="{{ asset('image/foto-produk.png') }}" alt=""
+                class="aspect-video md:rounded-r-full md:border-8 md:border-l-0 border-[#ff78c4] md:p-2 pl-0">
+        </div>
+        <div class="md:w-1/2 md:pr-4 mt-4 mb-6 md:mt-0 md:mb-0">
+            <section class="flex gap-2 md:flex-col md:block items-center justify-center mb-3 md:mb-0">
+                <div class="md:w-10 md:h-10 w-8 h-8 bg-gray-100 flex items-center justify-center rounded-md shadow"><i
+                        class="fa-solid fa-handshake text-base md:text-xl text-[#ff78c4] "></i></div>
+                <h2 class="text-base md:text-xl font-bold md:mt-4 md:mb-6 capitalize">Harga Terjangkau untuk Momen Spesial</h2>
+            </section>
+            <p class="text-xs text-justify md:text-left">Karena ukurannya yang mini, harga Bento Cake juga tidak terlalu tinggi. Hal ini membuatnya menjadi pilihan favorit bagi para pelajar dan mahasiswa yang ingin merayakan ulang tahun dengan anggaran terbatas. Rentang harga antara 55 ribu hingga 80 ribu rupiah, yang dipengaruhi oleh tingkat kesulitan desain, menjadikan Bento Cake terjangkau namun tetap memberikan sentuhan khusus pada momen spesial mereka.</p>
+        </div>
+    </section>
+    <section class="flex items-center flex-col md:flex-row md:-mt-2">
+        <div class="md:w-1/2">
+            <img src="{{ asset('image/Bolen.jpg') }}" alt=""
+                class=" aspect-video md:rounded-l-full md:border-8 md:border-r-0 border-[#ff78c4] md:p-2 pr-0 ">
+        </div>
+        <div class="md:w-1/2 md:pl-4 mt-4 mb-6 md:mt-0 md:mb-0">
+            <section class="flex gap-2 md:flex-col md:block items-center justify-center mb-3 md:mb-0">
+                <div class="md:w-10 md:h-10 w-8 h-8 bg-gray-100 flex items-center justify-center rounded-md shadow"><i
+                        class="fa-solid fa-medal text-base md:text-xl text-[#ff78c4] "></i></div>
+                <h2 class="text-base md:text-xl font-bold md:mt-4 md:mb-6 capitalize">Kualitas dan Kemudahan Tanpa Kompromi</h2>
+            </section>
+            <p class="text-xs text-justify md:text-left">Bento Cake juga dikenal dengan kualitas rasanya yang lezat dan tekstur yang sempurna, berkat bahan-bahan berkualitas tinggi. Selain itu, proses pemesanan yang mudah dan praktis memudahkan konsumen untuk mendapatkan kue yang mereka inginkan tanpa repot. Kombinasi keunikan, kualitas, dan harga yang terjangkau menjadikan Bento Cake pilihan utama bagi mereka yang mencari kue ulang tahun yang istimewa dan personal.</p>
+        </div>
+    </section>
+</div>
